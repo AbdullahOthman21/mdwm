919c7ca5fdbd00baba16e40e9bc8c57960775752
anon <anon@none.com>
2026-05-18 23:57:16 +0300

    Change project structure

---
+++ build.sh
@@ -0,0 +1,11 @@
+set -eux
+
+CFLAGS="\
+-pipe -std=c99 -pedantic -Wall -Wno-deprecated-declarations -O0 \
+-D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"6.8\" \
+-I/usr/include/freetype2 \
+"
+
+#test -f drw.o || gcc -c $CFLAGS drw.c
+
+gcc -o dwm $CFLAGS -Wunused-macros -lX11 -lfontconfig -lXft util.c drw.c dwm.c
