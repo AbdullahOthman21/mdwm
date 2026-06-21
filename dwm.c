f9571002493ebdba49d1c4b5819d0b649b289b04
anon <anon@none.com>
2026-06-21 13:20:11 +0300

    Remove compile-time check if all tags fit into an unsigned int bit array

--- dwm.c
+++ dwm.c
@@ -231,9 +231,6 @@ static Window root, wmcheckwin;
 /* configuration, allows nested code to access above variables */
 #include "config.h"
 
-/* compile-time check if all tags fit into an unsigned int bit array. */
-struct NumTags { char limitexceeded[LENGTH(tags) > 31 ? -1 : 1]; };
-
 /* function implementations */
 void
 applyrules(Client *c)
