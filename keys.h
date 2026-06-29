21cffdcd168b1bb3c57f34fa6074e1b85bc33909
anon <anon@none.com>
2026-06-29 14:57:13 +0300

    keys.h cleanup

--- keys.h
+++ keys.h
@@ -24,7 +24,7 @@ static const char *low_brightness[] = {
 
 static const char *mid_brightness[] = {
 	"sh", "-c",
-	"echo 5000 | sudo tee " brightness,
+	"echo 6000 | sudo tee " brightness,
 	NULL };
 
 static const char *screenshot[]        = { "sh", "-c", "maim    ~/$(date +%Y-%m-%d_%H-%M-%S).png", NULL };
@@ -37,30 +37,30 @@ static const char *mute[]              = { "sh", "-c", "pactl set-sink-volume @D
 
 #include <X11/XF86keysym.h>
 static const Key keys[] = {
-	/* modifier                     key           function        argument */
-	{ MODKEY,                       XK_p,         spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return,    spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_f,         spawn,          {.v = ff} },
-	{ MODKEY,                       XK_s,         spawn,          {.v = dpms} },
-	{ MODKEY,                       XK_j,         focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,         focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_h,         setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,         setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return,    zoom,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,         killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_w,         killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_space,     togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_q,         quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_BackSpace, killclient,     {0} },
-	{ 0,                            XK_Print,     spawn,          {.v = screenshot} },
-	{ MODKEY,                       XK_Print,     spawn,          {.v = screenshot_region} },
+	/* modifier         key           function        argument */
+	{ MODKEY,           XK_p,         spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask, XK_Return,    spawn,          {.v = termcmd } },
+	{ MODKEY,           XK_f,         spawn,          {.v = ff} },
+	{ MODKEY,           XK_s,         spawn,          {.v = dpms} },
+	{ MODKEY,           XK_j,         focusstack,     {.i = +1 } },
+	{ MODKEY,           XK_k,         focusstack,     {.i = -1 } },
+	{ MODKEY,           XK_h,         setmfact,       {.f = -0.05} },
+	{ MODKEY,           XK_l,         setmfact,       {.f = +0.05} },
+	{ MODKEY,           XK_Return,    zoom,           {0} },
+	{ MODKEY|ShiftMask, XK_c,         killclient,     {0} },
+	{ MODKEY|ShiftMask, XK_w,         killclient,     {0} },
+	{ MODKEY|ShiftMask, XK_space,     togglefloating, {0} },
+	{ MODKEY|ShiftMask, XK_q,         quit,           {0} },
+	{ MODKEY|ShiftMask, XK_BackSpace, killclient,     {0} },
+	{ 0,                XK_Print,     spawn,          {.v = screenshot} },
+	{ MODKEY,           XK_Print,     spawn,          {.v = screenshot_region} },
 
-	{ 0,                XF86XK_AudioMute,         spawn,          {.v = mute} },
-	{ 0,                XF86XK_AudioRaiseVolume,  spawn,          {.v = increase_volume} },
-	{ 0,                XF86XK_AudioLowerVolume,  spawn,          {.v = decrease_volume} },
-	{ 0,                XF86XK_MonBrightnessUp,   spawn,          {.v = high_brightness} },
-	{ MODKEY,           XK_d,                     spawn,          {.v = mid_brightness} },
-	{ 0,                XF86XK_MonBrightnessDown, spawn,          {.v = low_brightness} },
+	{ 0,        XF86XK_AudioMute,           spawn,    {.v = mute} },
+	{ 0,        XF86XK_AudioRaiseVolume,    spawn,    {.v = increase_volume} },
+	{ 0,        XF86XK_AudioLowerVolume,    spawn,    {.v = decrease_volume} },
+	{ 0,        XF86XK_MonBrightnessUp,     spawn,    {.v = high_brightness} },
+	{ MODKEY,   XK_d,                       spawn,    {.v = mid_brightness} },
+	{ 0,        XF86XK_MonBrightnessDown,   spawn,    {.v = low_brightness} },
 
 	TAGKEYS(XK_1, 0)
 	TAGKEYS(XK_2, 1)
