4abd1cb8f04a2ee65434568a1f62471afa92812a
anon <anon@none.com>
2026-07-18 13:10:50 +0300

    Change paths to scripts to absolute path

--- keys.h
+++ keys.h
@@ -3,13 +3,15 @@
 	{ MODKEY,                       KEY,      view,           {.i = TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.i = TAG} },
 
+#define S "/home/me/projects/dwm/scripts/"
+
 #include <X11/XF86keysym.h>
 static const Key keys[] = {
 	/* modifier         key           function        argument */
 	{ MODKEY,           XK_p,         spawn,          {.v = "dmenu_run" } },
 	{ MODKEY|ShiftMask, XK_Return,    spawn,          {.v = "st" } },
 	{ MODKEY,           XK_f,         spawn,          {.v = "firefox"} },
-	{ MODKEY,           XK_s,         spawn,          {.v = "dpms"} },
+	{ MODKEY,           XK_s,         spawn,          {.v = S"dpms"} },
 	{ MODKEY,           XK_j,         focusstack,     {.i = +1 } },
 	{ MODKEY,           XK_k,         focusstack,     {.i = -1 } },
 	{ MODKEY,           XK_h,         setmfact,       {.f = -0.05} },
@@ -20,15 +22,15 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask, XK_space,     togglefloating, {0} },
 	{ MODKEY|ShiftMask, XK_q,         quit,           {0} },
 	{ MODKEY|ShiftMask, XK_BackSpace, killclient,     {0} },
-	{ 0,                XK_Print,     spawn,          {.v = "screenshot" } },
-	{ MODKEY,           XK_Print,     spawn,          {.v = "screenshot_region"} },
+	{ 0,                XK_Print,     spawn,          {.v = S"screenshot" } },
+	{ MODKEY,           XK_Print,     spawn,          {.v = S"screenshot_region"} },
 
-	{ 0,        XF86XK_AudioMute,           spawn,    {.v = "mute"} },
-	{ 0,        XF86XK_AudioRaiseVolume,    spawn,    {.v = "increase_volume"} },
-	{ 0,        XF86XK_AudioLowerVolume,    spawn,    {.v = "decrease_volume"} },
-	{ 0,        XF86XK_MonBrightnessUp,     spawn,    {.v = "high_brightness"} },
-	{ MODKEY,   XK_d,                       spawn,    {.v = "mid_brightness"} },
-	{ 0,        XF86XK_MonBrightnessDown,   spawn,    {.v = "low_brightness"} },
+	{ 0,        XF86XK_AudioMute,           spawn,    {.v = S"mute"} },
+	{ 0,        XF86XK_AudioRaiseVolume,    spawn,    {.v = S"increase_volume"} },
+	{ 0,        XF86XK_AudioLowerVolume,    spawn,    {.v = S"decrease_volume"} },
+	{ 0,        XF86XK_MonBrightnessUp,     spawn,    {.v = S"high_brightness"} },
+	{ MODKEY,   XK_d,                       spawn,    {.v = S"mid_brightness"} },
+	{ 0,        XF86XK_MonBrightnessDown,   spawn,    {.v = S"low_brightness"} },
 
 	TAGKEYS(XK_1, 0)
 	TAGKEYS(XK_2, 1)
