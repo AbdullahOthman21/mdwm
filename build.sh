9bdec456548a4593d24d969a4637e5935f759bd4
anon <anon@none.com>
2026-07-27 21:22:02 +0300

    Add an install option in build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
+#!/bin/sh
+
 CFLAGS="\
 -pipe -std=c99 -Wall -Wno-deprecated-declarations -Wunused-macros -Os \
 -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L \
 -I/usr/include/freetype2 \
 "
 
-test -e drw.o || gcc -c $CFLAGS drw.c
+if [ ! -e drw.o ]; then
+	gcc -c $CFLAGS drw.c
+fi
 
 gcc -c $CFLAGS mdwm.c
 gcc -lX11 -lfontconfig -lXft mdwm.o drw.o -o mdwm && mv mdwm bin/
 rm -f mdwm.o
+
+if [ "$1" = install ]; then
+	mv bin/mdwm /usr/local/bin/
+fi
